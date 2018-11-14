c92db3f6543ee66887109f18f3f9197da1ffd2ec
Monirul <monirul.molla.cts@gmail.com>
2018-11-14 09:36:39 -0500

    2) *ngFor structural directive
    2) *ngFor structural directive
    
    		a) Create a class level property "devs" of type Developer[] array inside the bio.component.ts and instantiate the devs property inside the constructor.
    
    			devs:Developer[];
    			constructor() {
    				this.devs = [new Developer("Rick","Olson","Angular",2010),
    				             new Developer("John","Papa","Angular",2000)
    							];
    			}
    
    		b) add the following lines of html code to bio.component.html
    
    			<p><span class="columnHeader">:::::::::::::::::::Structural Directive *ngFor :::::::::::::::::::::</span> </p>
    			<div *ngFor="let dev of devs">
    				<p><span class="bold">First Name:</span> {{dev.firstName}}</p>
    				<p><span class="bold">Last Name:</span> {{dev.lastName}}</p>
    				<p><span class="bold">Favorite Language:</span> {{dev.favoriteLanguage}}</p>
    				<p><span class="bold">Year Started:</span> {{dev.yearStarted}}</p>
    				<hr>
    			</div>

--- src/app/bio/bio.component.ts
+++ src/app/bio/bio.component.ts
@@ -9,9 +9,13 @@ import { Developer } from '../developer';
 export class BioComponent implements OnInit {
   dev:Developer;
   dev1:Developer;
+  devs:Developer[];
   constructor() {
     this.dev = new Developer("Monirul","Molla","Java",2008);
     this.dev1 = new Developer("John","Doe","Angular",2015);
+    this.devs = [new Developer("Rick","Olson","Angular",2010),
+				             new Developer("John","Papa","Angular",2000)
+							];
   }
 
   ngOnInit() {
